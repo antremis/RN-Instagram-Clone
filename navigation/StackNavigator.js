341a337ff63bac326b4b2d16484ff96ca95135a2
Akash <akash7890123@gmail.com>
2022-02-09 16:49:47 +0530

    Iitialised project

---
+++ navigation/StackNavigator.js
@@ -0,0 +1,18 @@
+import {NavigationContainer} from "@react-navigation/native"
+import {createNativeStackNavigator} from "@react-navigation/native-stack"
+import Home from "../screens/Home"
+import React from "react"
+
+const Stack = createNativeStackNavigator()
+
+const StackNavigator = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name = "Home" component = {Home}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default StackNavigator
